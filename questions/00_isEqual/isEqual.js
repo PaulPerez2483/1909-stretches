00932d8f807c8ad91d3bd2a0beb3c9d3735eccb5
Paul Perez <colopaul@hotmail.com>
2019-09-27 20:40:34 -0400

    done

--- questions/00_isEqual/isEqual.js
+++ questions/00_isEqual/isEqual.js
@@ -3,6 +3,24 @@ function isEqual(obj1, obj2) {
   // run your tests by running `jest isEqual`
   // you can also run tests on every save by running
   // `jest --watch isEqual`
+  // console.log(obj1, obj2)
+    const one = Object.values(obj1);
+    const two = Object.values(obj2);
+
+    if(one.length !== two.length) {
+      return false;
+    }else {
+      for(let i = 0; i < one.length; i++) {
+        // console.log(one[i], two[i])
+        if(one[i] === two[i]) {
+          return true;
+        }else {
+          return false;
+        }
+      }
+    }
+
+return true
 }
 
 module.exports = { isEqual };
