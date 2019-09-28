f267475a86c1750bf94623293d1c7c5ff175a2c2
Paul Perez <colopaul@hotmail.com>
2019-09-27 20:40:08 -0400

    task hm

--- questions/01_largestDivisible/largestDivisible.js
+++ questions/01_largestDivisible/largestDivisible.js
@@ -4,7 +4,26 @@ write the largestDivisible function
 
 */
 
-function largestDivisible(arr, num) {}
+function largestDivisible(arr, num) {
+
+    if(arr.length <= 0 || num === 0) {
+        return null;
+    }
+    let x =  arr.filter(el => {
+        if(el % 3 === 0) {
+            return el;
+        }
+    })
+    
+    if(x.length > 0) {
+        console.log(x)
+        return Math.max(...x)
+    }
+
+return null
+
+}
+
 
 // console.log(largestDivisible([1, 2, 3, 4, 5, 6, 7], 3));//6
 // console.log(largestDivisible([1], 2));//null
