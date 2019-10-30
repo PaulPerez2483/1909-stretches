9c0d56c4730c0a3b1a0d68c280e83080944513ed
Paul Perez <colopaul@hotmail.com>
2019-10-30 19:01:51 -0400

    done

--- questions/13_multiply/multiply.test.js
+++ questions/13_multiply/multiply.test.js
@@ -4,20 +4,20 @@ describe('multiply testing', () => {
   test('it should ensure it receives two numbers', () => {
     // test some values that should fail
 
-    expect(() => multiply(/*fill this in*/)).toThrow();
+    expect(() => multiply('string', 2)).toThrow();
 
-    expect(() => multiply(/*fill this in*/)).toThrow();
+    expect(() => multiply(4, ['num'])).toThrow();
   });
 
   test('it returns the product of two numbers', () => {
     // test the type of the returned value
-    expect(/*fill this in*/).toBe(/*fill this in*/);
+    expect(multiply(3, 3)).toBe(9);
 
     // test that the returned value is correct
-    expect(/*fill this in*/).toBe(/*fill this in*/);
+    expect(multiply(1, 1)).toBe(1);
 
     // test some other values
-    expect(/* fill this in */).toBe(/* fill this in */);
+    expect(multiply(2, 2)).toBe(4);
   });
 
   test('it is not hardcoded (hint: use random numbers)', () => {
