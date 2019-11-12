80faabb014191dab7b7037ac1da958d375771d77
Paul Perez <colopaul@hotmail.com>
2019-11-12 13:27:52 -0500

    ..

--- questions/14_mockingbird/mockingBird.js
+++ questions/14_mockingbird/mockingBird.js
@@ -1,18 +1,20 @@
 const repeater = (func, n) => {
   // repeater runs a function n times and returns an array of its outputs
   let newArr = [];
-  if (typeof func === 'function') {
-    func()
-
-  } else if (Array.isArray(func)) {
-    for (let i = 0; i < n; i++) {
+  for (let i = 0; i < n; i++) {
+    if (typeof func === 'function') {
       newArr.push(func());
-    };
+    }
+    else {
+      newARr.push([]);
+    }
   }
-
-
   console.log(newArr)
-
+  return newArr;
 };
+
+
+
+
 
 module.exports = { repeater };
