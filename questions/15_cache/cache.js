b77ddfd8ea243df4dc11ef4ab42b87655a646dd5
Paul Perez <colopaul@hotmail.com>
2019-11-13 19:15:58 -0500

    4 of 5

--- questions/15_cache/cache.js
+++ questions/15_cache/cache.js
@@ -2,10 +2,15 @@ function cache(fn) {
     if (typeof fn !== 'function') {
         throw "Input must be a function.";
     }
+    const obj = {};
     return function (num) {
         console.log(num)
-
-        return fn(num)
+        if (num in obj) {
+            return obj[num]
+        } else {
+            obj[num] = fn(num);
+            return obj[num];
+        }
     }
 
 }
