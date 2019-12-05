efb2cee7ccca64c2656316416d8bfd082abda93d
Paul Perez <colopaul@hotmail.com>
2019-12-04 19:04:52 -0500

    done

--- questions/19_calcluator/calculator.js
+++ questions/19_calcluator/calculator.js
@@ -1,13 +1,27 @@
 class Calculator {
+	constructor() {
+		this.value = 0;
+	}
 	add(num) {
-		console.log(num);
+		this.value = num + this.value;
+		return this;
 	}
 
-	subtract() {}
-	divide() {}
-	multiply() {}
+	subtract(num) {
+		this.value = this.value - num;
+		return this;
+	}
+	divide(num) {
+		this.value = this.value / num;
+		return this;
+	}
+	multiply(num) {
+		this.value = this.value * num;
+		return this;
+	}
 	clear(num) {
-		value: num;
+		this.value = 0;
+		return this;
 	}
 }
 
